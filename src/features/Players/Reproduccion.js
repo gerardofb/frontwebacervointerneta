fe49c76fc842b59b2575c0078f0e91c3c0a90f31
Gerardo Flores Barrie <gerardo.flores@impi.gob.mx>
2022-06-29 22:45:58 -0500

    Estilos a página de reproducción individual

--- src/features/Players/Reproduccion.js
+++ src/features/Players/Reproduccion.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { BigPlayButton, ControlBar, LoadingSpinner, Player, PlayToggle } from 'video-react'
 import { useParams } from 'react-router-dom'
+import SearchBar from '../SearchBar';
+import NavBar from '../NavBar';
 let items = [
     { titulo: "Gerardo Flores Barrie", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet suscipit mi. Integer lacinia nisl sit amet sapien porta, nec posuere ante dictum. Aliquam erat volutpat. Pellentesque sem purus, laoreet at quam eget, ullamcorper efficitur mi. Integer pretium fringilla placerat. Suspendisse bibendum, neque quis vestibulum interdum, tortor metus scelerisque." },
     { titulo: "Zosim Silva Gómez", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan, orci vitae efficitur placerat, quam odio tempor arcu, non commodo elit velit quis augue. Morbi luctus pellentesque diam id ultrices. Proin massa augue, fermentum eget nibh nec, semper fringilla ipsum. Duis ut ipsum erat. Etiam sollicitudin pulvinar augue, et dapibus erat interdum non. Aenean et lacus dignissim, faucibus justo eu, maximus ex. Quisque eu justo eget nisl eleifend mollis sit amet eget arcu. Quisque tincidunt, nibh ut lobortis sollicitudin, metus orci eleifend metus, ac viverra quam mi ut elit. Quisque aliquet tincidunt nisi. Nullam viverra purus at odio dictum viverra. Quisque eleifend magna eget turpis hendrerit, eget molestie dolor tincidunt. Phasellus at nisi massa. In sollicitudin blandit rhoncus. Duis mattis dictum mi, eget consequat nibh. Morbi vulputate, purus molestie sodales gravida, nisi odio efficitur sem, vitae ornare lacus est eget massa. Sed ornare massa lectus, non tincidunt purus rhoncus vel. Aenean cursus erat at tristique faucibus." },
@@ -30,41 +32,49 @@ export const AutoComments = () => {
         const { scrollTop, offsetHeight } = document.documentElement;
         const { innerHeight } = window;
         const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
-        console.log(Math.round(scrollTop) + innerHeight,offsetHeight);
-        if(bottomOfWindow){
+        console.log(Math.round(scrollTop) + innerHeight, offsetHeight);
+        if (bottomOfWindow && items.length < 500) {
             let item = items[Math.floor(Math.random() * items.length)];
             items = items.concat(item);
             setItems(items)
             console.log('estableciendo ' + elems.length + " " + items.length);
         }
     }
-    
+
     return (
 
 
         <div className='player-individual' onScroll={handleScroll}>
-            <h2>
-                {titulo}
-            </h2>
-            <Player
-                ref={player => {
-                    player = player;
-                }}>
-                <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"></source>
-                <ControlBar></ControlBar>
-                <LoadingSpinner></LoadingSpinner>
-            </Player>
-            <div className="scroll-list" ref={bottomRef} >
-                {elems &&
-                    elems.map((item, index) => (
-                        <div key={index}>
-                            <h4>{item.titulo}</h4>
-                            <p>{`${index + 1}. ${item.content}`}</p>
-                        </div>
-                    ))}
-                <div className="list-bottom"></div>
+            <div style={{ backgroundColor: 'black', height: '100px' }}>
+                <NavBar></NavBar>
+                <SearchBar style={{ width: '60%' }}></SearchBar>
             </div>
-            <div style={{height:"10px"}} />
+            <h2>
+                Reproduciendo: {titulo}
+            </h2>
+            <div className='player-container'>
+                <Player
+                    ref={player => {
+                        player = player;
+                    }}>
+                    <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"></source>
+                    <ControlBar></ControlBar>
+                    <LoadingSpinner></LoadingSpinner>
+                </Player>
+            </div>
+            <div className='content-player'>
+                <div className="scroll-list" ref={bottomRef} >
+                    {elems &&
+                        elems.map((item, index) => (
+                            <div key={index}>
+                                <h4>{item.titulo}</h4>
+                                <p>{`${index + 1}. ${item.content}`}</p>
+                            </div>
+                        ))}
+                    <div className="list-bottom"></div>
+                </div>
+                <div className='categories-player'></div>
+            </div>
         </div>
 
 
