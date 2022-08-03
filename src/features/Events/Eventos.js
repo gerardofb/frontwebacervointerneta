2d4e8f6ec69390f256a0ea20213fc9696335fc80
Gerardo Flores Barrie <gerardo.flores@impi.gob.mx>
2022-08-03 02:22:38 -0500

    Avance de página de eventos

--- src/features/Events/Eventos.js
+++ src/features/Events/Eventos.js
@@ -3,19 +3,19 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import NavBar from '../NavBar';
 const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 const dayOfWeek = ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sab'];
+const cssMeses = ['ene', 'feb', 'mar', 'abr','may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic']
 const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 const Eventos = () => {
     return (
-        <div className="eventos-main-container">
-
-            <Parallax pages={13}>
+        <div>
+            <Parallax pages={13} className="eventos-main-container">
                 <ParallaxLayer offset={0} speed={0}>
                     <div style={{ backgroundColor: 'black', height: '100px' }}>
                         <NavBar></NavBar>
                     </div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0.16} speed={1}>
-                    <div style={{height:'85vh', background:'pink'}} />
+                    <div style={{height:'85vh', background:'#feecb8'}} />
                 </ParallaxLayer>
                 <ParallaxLayer sticky={{ start: 1, end: 12 }}>
                     <div className="calendario-eventos-principal">
@@ -51,7 +51,7 @@ const Eventos = () => {
                 {
                     meses && meses.length > 0 ? meses.map((mes, index) => {
                         return <ParallaxLayer offset={index+1} key={index} speed={1}>
-                            <div className="mes-evento-main">
+                            <div className={"mes-evento-main "+cssMeses[index]}>
                             <h1>{mes}</h1>
                             </div>
                         </ParallaxLayer>
