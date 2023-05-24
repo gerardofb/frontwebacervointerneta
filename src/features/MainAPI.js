5dfaa7e0d63303c574b42441e0bc0a8c71128277
Gerardo Flores Barrie <gerardo.flores@impi.gob.mx>
2023-05-24 14:44:28 -0600

    Se cambia host por suspensión temporal de cuenta

--- src/features/MainAPI.js
+++ src/features/MainAPI.js
@@ -1,3 +1,3 @@
 export function getBaseAdressApi(){
-    return 'http://52.53.166.12:5000/' //'http://localhost:8000/' //'http://localhost:5000/' //'http://52.53.166.12:5000/' 
+    return 'http://54.153.125.23:5000/' //'http://localhost:8000/' //'http://localhost:5000/' //'http://52.53.166.12:5000/' 
 }
