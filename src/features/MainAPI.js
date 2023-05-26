7847c8a4c81ba0d67b422b0749406f74286bd7e3
gerardofb <gerardo.v.flores@gmail.com>
2023-05-25 22:02:23 -0600

    Se cambia api en https por nombre de subdominio

--- src/features/MainAPI.js
+++ src/features/MainAPI.js
@@ -1,3 +1,3 @@
 export function getBaseAdressApi(){
-    return 'https://54.153.125.23/' //'http://localhost:8000/' //'http://localhost:5000/' //'http://52.53.166.12:5000/' 
+    return 'https://api.acervo-audiovisual-interneta.org/' //'http://localhost:8000/' //'http://localhost:5000/' //'http://52.53.166.12:5000/' 
 }
