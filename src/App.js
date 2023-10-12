3403fcb968ae9ba3037353b31eac92c21b9c499a
gerardofb <gerardo.v.flores@gmail.com>
2023-10-12 06:10:07 -0600

    Se agrega verificación de dispositivo móvil y redirección a otro dominio

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ import { useState } from 'react';
 import { getBaseAdressApi } from './features/MainAPI'
 import ReactGA from "react-ga4";
 import HelmetMetaData from './features/HelmetMetaData';
+import { isMobile } from 'react-device-detect';
 
 
 library.add(faBook, faHouse, faTape, faForward, faCommentDots, faVolumeHigh, faCalendarDays, faUserCheck, faEnvelope,
@@ -73,9 +74,16 @@ history.push = args => {
     cachedPush(args)
   }
 }
-
+const BANDERA_MOVILES = { bandera: true, redireccion: "https://api.acervo-audiovisual-interneta.org" };
+// falso = mayor que tableta, verdadero = menor o igual que tableta;
 function App() {
   const location = window.location;
+  // SE VERIFICA REDIRECCIÒN A SITIO MÒVIL, EN CASO DE SER MENOR O IGUAL A TABLETA EL RESTO DEL CÓDIGO NO CORRERÁ
+  if (isMobile === BANDERA_MOVILES.bandera &&
+    window.matchMedia("(any-pointer:coarse)").matches === BANDERA_MOVILES.bandera
+    && window.matchMedia("only screen and (max-width: 480px)").matches === BANDERA_MOVILES.bandera) {
+    window.location = BANDERA_MOVILES.redireccion
+  }
   const [videosPopulated, setVideosPopulated] = useState(null);
   const [categoriasService, setCategoriasService] = useState([]);
   ReactGA.initialize("G-QHDVTJ0KW9");
@@ -143,155 +151,155 @@ function App() {
   //console.log('los videos categorizados en app ',videosPopulated);
   return (
     <>
-    <HelmetMetaData
-    image="https://api.acervo-audiovisual-interneta.org/static/img/logo_nuevo_negro.png"
-    title="Acervo Audiovisual Interneta | Memoria de las y los invisibles |"
-    description='Clip introductorio del acervo. Durante tres décadas, el colectivo de videastas estructuramos el Acervo Audiovisual InterNeta. Genera un espacio online y offline, para retroalimentar las creaciones producidas, en comunidades urbanas, indígenas, y movimientos contraculturales.' 
-    keywords={[
-      "Arte Urbano", 
-      "Generación Trasparente", 
-      "Memoria y Movimiento Urbano Popular", 
-      "Movimientos Sociales y Políticos", 
-      "Movimientos Contraculturales", 
-      "Pueblos Originarios e Indígenas", 
-      "Movimientos en Defensa del Territorio"
-    ]}></HelmetMetaData>
-    <BrowserRouter>
-      <div className="App">
-        <div className='container-menu-main'>
-          <SideBar></SideBar>
+      <HelmetMetaData
+        image="https://api.acervo-audiovisual-interneta.org/static/img/logo_nuevo_negro.png"
+        title="Acervo Audiovisual Interneta | Memoria de las y los invisibles |"
+        description='Clip introductorio del acervo. Durante tres décadas, el colectivo de videastas estructuramos el Acervo Audiovisual InterNeta. Genera un espacio online y offline, para retroalimentar las creaciones producidas, en comunidades urbanas, indígenas, y movimientos contraculturales.'
+        keywords={[
+          "Arte Urbano",
+          "Generación Trasparente",
+          "Memoria y Movimiento Urbano Popular",
+          "Movimientos Sociales y Políticos",
+          "Movimientos Contraculturales",
+          "Pueblos Originarios e Indígenas",
+          "Movimientos en Defensa del Territorio"
+        ]}></HelmetMetaData>
+      <BrowserRouter>
+        <div className="App">
+          <div className='container-menu-main'>
+            <SideBar></SideBar>
+          </div>
+          <Switch>
+            <Route path='/' exact>
+              <ThemeProvider>
+                <Home />
+              </ThemeProvider>
+            </Route>
+            {<Route path="/Categorias" exact
+              render={({ location, search }) => {
+                return (
+                  <ThemeProvider>
+                    <Flipper
+                      flipKey={`${location.pathname}-${location.search}`}
+                      decisionData={{
+                        location,
+                        search
+                      }}
+                    >
+                      <IndexPage />
+                    </Flipper>
+                  </ThemeProvider>
+                )
+              }}
+            />}
+            <Route path="/Categorias/:set/:focusedVideo">
+              <ThemeProvider>
+                <VideoSetPage videos={videosPopulated}></VideoSetPage>
+              </ThemeProvider>
+            </Route>
+            <Route path="/Reproduccion/:video/" exact>
+              <ThemeProvider>
+                <AutoComments></AutoComments>
+              </ThemeProvider>
+            </Route>
+            <Route path='/Autobiograficos/:relato' exact>
+              <ThemeProvider>
+                <Autobiograficos></Autobiograficos>
+              </ThemeProvider>
+            </Route>
+            <Route path="/Eventos/:evento" exact>
+              <ThemeProvider>
+                <Eventos></Eventos>
+              </ThemeProvider>
+            </Route>
+            <Route path="/Eventos/ListadoEventos/:tipo" exact>
+              <ThemeProvider>
+                <ListadoEventos></ListadoEventos>
+              </ThemeProvider>
+            </Route>
+            <Route path="/Eventos/ListadoProximosEventos/:tipo" exact>
+              <ThemeProvider>
+                <ListadoEventosProximos></ListadoEventosProximos>
+              </ThemeProvider>
+            </Route>
+            <Route path="/Eventos/EventosMasVisitados/:tipo" exact>
+              <ThemeProvider>
+                <EventosMasVisitados></EventosMasVisitados>
+              </ThemeProvider>
+            </Route>
+            <Route path="/ListadoReproduccion/:tipo" exact>
+              <ThemeProvider>
+                <ListadoVideosFavoritos></ListadoVideosFavoritos>
+              </ThemeProvider>
+            </Route>
+            <Route path="/ListadoReproduccionVisitados/:tipo" exact>
+              <ThemeProvider>
+                <ListadoVideosMasVisitados></ListadoVideosMasVisitados>
+              </ThemeProvider>
+            </Route>
+            <Route path="/ListadoRelatosFavoritos/:tipo" exact>
+              <ThemeProvider>
+                <ListadoRelatosFavoritos></ListadoRelatosFavoritos>
+              </ThemeProvider>
+            </Route>
+            <Route path="/ListadoRelatosMasVisitados/:tipo" exact>
+              <ThemeProvider>
+                <ListadoMasVisitadosRelatos></ListadoMasVisitadosRelatos>
+              </ThemeProvider>
+            </Route>
+            <Route path="/BusquedaEstandar">
+              <ThemeProvider>
+                <BusquedaEstandar></BusquedaEstandar>
+              </ThemeProvider>
+            </Route>
+            <Route path="/Registro" exact>
+              <ThemeProvider>
+                <Register></Register>
+              </ThemeProvider>
+            </Route>
+            <Route path="/Login" exact>
+              <ThemeProvider>
+                <Login></Login>
+              </ThemeProvider>
+            </Route>
+            <Route path="/CerrarSesion">
+              <ThemeProvider>
+                <Logout></Logout>
+              </ThemeProvider>
+            </Route>
+            <Route path="/Contacto">
+              <ThemeProvider>
+                <FormaContacto></FormaContacto>
+              </ThemeProvider>
+            </Route>
+            <Route path="/VerificaRegistro">
+              <ThemeProvider>
+                <ValidarRegistro></ValidarRegistro>
+              </ThemeProvider>
+            </Route>
+            <Route path="/AvisoPivacidad">
+              <ThemeProvider>
+                <AvisoPrivacidad></AvisoPrivacidad>
+              </ThemeProvider>
+            </Route>
+            <Route path="/Perfil">
+              <ThemeProvider>
+                <Perfil></Perfil>
+              </ThemeProvider>
+            </Route>
+            <Route path="/QuienesSomos">
+              <ThemeProvider>
+                <QuienesSomos></QuienesSomos>
+              </ThemeProvider>
+            </Route>
+            <Route path="*">
+              <ThemeProvider>
+                <Pagina_404></Pagina_404>
+              </ThemeProvider>
+            </Route>
+          </Switch>
         </div>
-        <Switch>
-          <Route path='/' exact>
-            <ThemeProvider>
-              <Home />
-            </ThemeProvider>
-          </Route>
-          {<Route path="/Categorias" exact
-            render={({ location, search }) => {
-              return (
-                <ThemeProvider>
-                  <Flipper
-                    flipKey={`${location.pathname}-${location.search}`}
-                    decisionData={{
-                      location,
-                      search
-                    }}
-                  >
-                    <IndexPage />
-                  </Flipper>
-                </ThemeProvider>
-              )
-            }}
-          />}
-          <Route path="/Categorias/:set/:focusedVideo">
-            <ThemeProvider>
-              <VideoSetPage videos={videosPopulated}></VideoSetPage>
-            </ThemeProvider>
-          </Route>
-          <Route path="/Reproduccion/:video/" exact>
-            <ThemeProvider>
-              <AutoComments></AutoComments>
-            </ThemeProvider>
-          </Route>
-          <Route path='/Autobiograficos/:relato' exact>
-            <ThemeProvider>
-              <Autobiograficos></Autobiograficos>
-            </ThemeProvider>
-          </Route>
-          <Route path="/Eventos/:evento" exact>
-            <ThemeProvider>
-              <Eventos></Eventos>
-            </ThemeProvider>
-          </Route>
-          <Route path="/Eventos/ListadoEventos/:tipo" exact>
-            <ThemeProvider>
-              <ListadoEventos></ListadoEventos>
-            </ThemeProvider>
-          </Route>
-          <Route path="/Eventos/ListadoProximosEventos/:tipo" exact>
-            <ThemeProvider>
-              <ListadoEventosProximos></ListadoEventosProximos>
-            </ThemeProvider>
-          </Route>
-          <Route path="/Eventos/EventosMasVisitados/:tipo" exact>
-            <ThemeProvider>
-              <EventosMasVisitados></EventosMasVisitados>
-            </ThemeProvider>
-          </Route>
-          <Route path="/ListadoReproduccion/:tipo" exact>
-            <ThemeProvider>
-              <ListadoVideosFavoritos></ListadoVideosFavoritos>
-            </ThemeProvider>
-          </Route>
-          <Route path="/ListadoReproduccionVisitados/:tipo" exact>
-            <ThemeProvider>
-              <ListadoVideosMasVisitados></ListadoVideosMasVisitados>
-            </ThemeProvider>
-          </Route>
-          <Route path="/ListadoRelatosFavoritos/:tipo" exact>
-            <ThemeProvider>
-              <ListadoRelatosFavoritos></ListadoRelatosFavoritos>
-            </ThemeProvider>
-          </Route>
-          <Route path="/ListadoRelatosMasVisitados/:tipo" exact>
-            <ThemeProvider>
-              <ListadoMasVisitadosRelatos></ListadoMasVisitadosRelatos>
-            </ThemeProvider>
-          </Route>
-          <Route path="/BusquedaEstandar">
-            <ThemeProvider>
-              <BusquedaEstandar></BusquedaEstandar>
-            </ThemeProvider>
-          </Route>
-          <Route path="/Registro" exact>
-            <ThemeProvider>
-              <Register></Register>
-            </ThemeProvider>
-          </Route>
-          <Route path="/Login" exact>
-            <ThemeProvider>
-              <Login></Login>
-            </ThemeProvider>
-          </Route>
-          <Route path="/CerrarSesion">
-            <ThemeProvider>
-              <Logout></Logout>
-            </ThemeProvider>
-          </Route>
-          <Route path="/Contacto">
-            <ThemeProvider>
-              <FormaContacto></FormaContacto>
-            </ThemeProvider>
-          </Route>
-          <Route path="/VerificaRegistro">
-            <ThemeProvider>
-              <ValidarRegistro></ValidarRegistro>
-            </ThemeProvider>
-          </Route>
-          <Route path="/AvisoPivacidad">
-            <ThemeProvider>
-              <AvisoPrivacidad></AvisoPrivacidad>
-            </ThemeProvider>
-          </Route>
-          <Route path="/Perfil">
-            <ThemeProvider>
-              <Perfil></Perfil>
-            </ThemeProvider>
-          </Route>
-          <Route path="/QuienesSomos">
-            <ThemeProvider>
-              <QuienesSomos></QuienesSomos>
-            </ThemeProvider>
-          </Route>
-          <Route path="*">
-            <ThemeProvider>
-              <Pagina_404></Pagina_404>
-            </ThemeProvider>
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
     </>
   );
 }
